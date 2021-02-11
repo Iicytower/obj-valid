3ba7f2b4c2b2801b7b48570dff609e5362936a1d
Iicytower <szymonfirkowicz@gmail.com>
2021-02-11 13:44:40 +0100

    add file

---
+++ class.js
@@ -0,0 +1,11 @@
+import createSchema from './createSchema.js';
+import checkSchema from './checkSchema.js';
+
+
+class Schema {
+
+  
+
+}
+
+export default Schema;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import configPrototype from './configPrototype.js';
+import Schema from './class.js'
 
 export {
   configPrototype,
-
+  Schema,
+  
 }
