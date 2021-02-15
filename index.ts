b13296ccbc035934d3f8e04bdede791affb741ea
Iicytower <szymonfirkowicz@gmail.com>
2021-02-15 11:43:34 +0100

    change import to export in index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,4 @@
-import { configPrototype } from './configPrototype';
-import { Schema } from './class'
-import { createSchema } from "./createSchema";
-import { checkSchema } from "./checkSchema";
-
-export {
-  configPrototype,
-  Schema,
-  checkSchema,
-  createSchema,
-}
+export { configPrototype } from './configPrototype';
+export { Schema } from './class'
+export { createSchema } from "./createSchema";
+export { checkSchema } from "./checkSchema";
