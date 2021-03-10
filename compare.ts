6b3a625356ad79e352af69f557a9d7234a938bd8
Iicytower <szymonfirkowicz@gmail.com>
2021-03-10 13:49:41 +0100

    fix problem wit if statement in compare func

--- compare.ts
+++ compare.ts
@@ -7,18 +7,23 @@ export const compare = (obj1: Record<string, any>, obj2: Record<string, any>): C
     score: true,
     wrongProperties: [],
   };
+
   for (const key in obj1) {
     if (!Object.prototype.hasOwnProperty.call(obj1, key)) continue;
       if(kindOf(obj1[key]) === 'object'){
-        if(!compare(obj1[key], obj2[key])) {
+
+        if(!compare(obj1[key], obj2[key]).score) {
           result.score = false;
           result.wrongProperties.push(String(key));
         }
+
       }else{
+
         if(obj1[key] !== obj2[key]) {
           result.score = false;
           result.wrongProperties.push(String(key));
         }
+
       }
   }
   return result;
