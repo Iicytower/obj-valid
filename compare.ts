591b2805182a4cda6f674994554f62324728e839
Iicytower <szymonfirkowicz@gmail.com>
2021-04-29 13:45:35 +0200

    add feature

--- compare.ts
+++ compare.ts
@@ -2,7 +2,7 @@ import kindOf from 'kind-of';
 import { CompareResult } from './interfaces';
 
 
-export const compare = (obj1: Record<string, any>, obj2: Record<string, any>): CompareResult => {
+export const compare = (schema: Record<string, any>, newSchema: Record<string, any>): CompareResult => {
 
   // function basis taken from https://javascript.plainenglish.io/4-ways-to-compare-objects-in-javascript-97fe9b2a949c
 
@@ -11,37 +11,46 @@ export const compare = (obj1: Record<string, any>, obj2: Record<string, any>): C
     wrongProperties: [],
   };
 
-  const props1: string[] = Object.getOwnPropertyNames(obj1);
-  const props2: string[] = Object.getOwnPropertyNames(obj2);
+  const doesNotRequirePropertiesExist: boolean = schema.hasOwnProperty("notRequireProperties");
+  console.log(doesNotRequirePropertiesExist);
 
-  if (props1.length != props2.length) {
-    result.score = false;
-    return result;
-  }
+  const props1: string[] = Object.getOwnPropertyNames(schema);
+  const props2: string[] = Object.getOwnPropertyNames(newSchema);
+
+  // if (props1.length != props2.length) {
+  //   result.score = false;
+  //   return result;
+  // }
 
   for (let i = 0; i < props1.length; i++) {
     const prop = props1[i];
+    if(prop === 'notRequireProperties') continue; 
 
     if (
       (props1[0] === "type" && props1[1] === "value") &&
       (props2[0] === "type" && props2[1] === "value")
     ) {
-      if(obj1.type === 'array' && obj1.value === 'any'){
-        obj1.value = obj2.value;
+      if(schema.type === 'array' && schema.value === 'any'){
+        schema.value = newSchema.value;
       }
-      if(obj2.type === 'array' && obj2.value === 'any'){
-        obj2.value = obj1.value;
+      if(newSchema.type === 'array' && newSchema.value === 'any'){
+        newSchema.value = schema.value;
       }
     }
 
-    const bothAreObjects: boolean = (kindOf(obj1[prop]) === 'object' && kindOf(obj2[prop]) === 'object');
+    const bothAreObjects: boolean = (kindOf(schema[prop]) === 'object' && kindOf(newSchema[prop]) === 'object');
 
     if (
-      (!bothAreObjects && (obj1[prop] !== obj2[prop])) ||
-      (bothAreObjects && !compare(obj1[prop], obj2[prop]).score)
+      (!bothAreObjects && (schema[prop] !== newSchema[prop])) ||
+      (bothAreObjects && !compare(schema[prop], newSchema[prop]).score)
     ) {
+
+      if(doesNotRequirePropertiesExist && schema.notRequireProperties.indexOf(prop) !== -1){
+        continue;
+      }
       result.score = false;
       result.wrongProperties.push(prop);
+
     }
   }
   return result;
